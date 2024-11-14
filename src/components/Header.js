45eaaf77164f5330be5899cb8ad5f1346968e885
suryanshpandey61 <suryansh.knipss061@gmail.com>
2024-11-14 20:26:35 +0530

    navbar reponsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,60 +10,57 @@ function Header() {
   return (
    
     //  top level div 
-    <div className='w-full  overflow-hidden flex justify-between'>
-     
-       {/* left image div  */}
-        <div className='w-[180px] h-[50px] pt-[25px] pl-[50px]'>
-          <img src={Capa5}/>   
-        </div>   
+<div className="w-full overflow-hidden flex flex-col lg:flex-row justify-between items-center lg:items-start px-4 py-4">
+  {/* Left image div */}
+  <div className="w-full lg:w-[180px] h-[50px] pt-[25px] pl-0 lg:pl-[50px] mb-4 lg:mb-0 flex justify-center">
+    <img src={Capa5} className="max-w-full" />
+  </div>
 
-        {/* rigth div with 5 things    */}
-        <div className='flex pt-[20px] w-[555px]'>
+  {/* Right div with 5 things */}
+  <div className="flex flex-col lg:flex-row pt-[20px] w-full lg:w-[555px] gap-4 lg:gap-6 items-center lg:items-start">
 
-            {/* bell-icon div  */}
-            <div className='relative'>
-                <img src={LivePulseAniam} className='relative left-[-10px] top-[-5px] z-10' />
-                <img src={Vector} className=' absolute w-[18px] h-[23px] top-[14px] '/>
-            </div>
+    {/* Bell-icon div */}
+    <div className="relative flex justify-center">
+      <img src={LivePulseAniam} className="relative left-[-10px] top-[-5px] z-10" />
+      <img src={Vector} className="absolute w-[18px] h-[23px] top-[14px]" />
+    </div>
 
-            {/* robinos coin div  */}
-            <div className='flex border rounded-md border-slate-800 w-[120px] '>
-                {/* left side two img  */}
-                <div className='pl-2'>
-                    <img src={ThreeColorRobinos} className=' relative w-[24px] h-[24px] top-[13px] '/>
-                    <p className='relative text-white left-8 font-clash font-medium leading-[17.22px] top-[-9px] w-[38px]'>Telos</p>
-                    
-                </div>
+    {/* Robinos coin div */}
+    <div className="flex border rounded-md border-slate-800  w-full lg:w-[120px] h-[50px] justify-between items-center px-2 py-1">
+      {/* Left side two images */}
+      <div className="flex items-center space-x-2">
+        <img src={ThreeColorRobinos} className="w-[24px] h-[24px]" />
+        <p className="text-white font-clash font-medium text-sm">Telos</p>
+      </div>
 
-                {/* right down logo div  */}
-                <div>
-                    <img src={DownArrow} className=' w-[10px] h-[5px] relative top-[22px] left-[55px] '/>
-                </div>
-               
-            </div>
+      {/* Right down arrow */}
+      <div>
+        <img src={DownArrow} className="w-[10px] h-[5px]" />
+      </div>
+    </div>
 
-            {/* 3 numbered div  */}
-            <div className='text-white border rounded-md border-slate-800 flex gap-x-5 ml-2'>
+    {/* 3 numbered div */}
+    <div className="text-white border pb-3  rounded-md border-slate-800 justify-between flex gap-x-5 lg:gap-x-4 w-full lg:w-auto">
+      
+      <div className="flex justify-center w-[87px] bg-[#131313] items-center h-[30px] mt-2 rounded-md px-1">
+        <img src={LivePulseAniam} className="w-[18px] h-[18px]" />
+        <p className="font-clash font-medium leading-[17.22px] text-[14px]">13,580</p>
+      </div>
 
-                <div className='flex justify-center w-[87px] bg-[#131313] items-center  h-[30px] mt-2 ml-1 rounded-md px-1 '>
-                    <img src={LivePulseAniam} className=''/>
-                    <p className='left-[-15px] relative font-clash font-medium leading-[17.22px] text-[14px]'>13,580</p>
-                </div>
+      <div className="flex gap-x-1 justify-center w-[87px] bg-[#131313] items-center h-[30px] mt-2 rounded-md px-1">
+        <img src={Star} className="w-[18px] h-[18px]" />
+        <p className="font-clash font-medium leading-[17.22px] text-[14px]">1,210</p>
+      </div>
 
-                <div  className='flex gap-x-1 justify-center w-[87px] bg-[#131313] items-center  h-[30px] mt-2  rounded-md px-1 '>
-                    <img src={Star}/>
-                    <p className='font-clash font-medium leading-[17.22px] text-[14px]'>1,210</p>
-                </div>
-
-                <div className='flex items-center pr-2'>
-                  <p className='font-clash font-medium leading-[17.22px] text-[14px] flex items-center'>0x4cd..edEF</p>
-                </div>
-            </div>
-
-        </div>
-
+      <div className="flex items-center pr-2">
+        <p className="font-clash font-medium leading-[17.22px] text-[14px]">0x4cd..edEF</p>
+      </div>
 
     </div>
+
+  </div>
+</div>
+
   )
 }
 
